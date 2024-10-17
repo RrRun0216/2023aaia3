06d84b86bfd3287678de0ec22ea857530b7d9354
RrRun0216 <144758468+RrRun0216@users.noreply.github.com>
2024-10-17 12:53:23 +0800

    Delete week03 directory

--- week03/week03-2.cpp
+++
@@ -1,8 +0,0 @@
-///week03-2
-#include <stdio.h>
-int main()
-{
-    printf("%d\n", 'a');///單引號，單一個字母
-    printf("%d\n", "a");///雙引號，很多字母、字串、字元陣列、指標
-    if('a' == 97) printf("原來'a'的值，就是97\n");
-}
--- week03/week03-4.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        //https://cplusplus.com/reference/string/string/find/
-        return haystack.find(needle);
-    }
-};
